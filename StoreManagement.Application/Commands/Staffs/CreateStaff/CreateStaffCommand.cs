a3d46dd8b8cd93db5ecd9a44ce9016afadef835c
anh.ton <anh.ton@aegona.com>
2024-05-22 21:38:56 +0700

    implement sample staff command

---
+++ StoreManagement.Application/Commands/Staffs/CreateStaff/CreateStaffCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+
+namespace StoreManagement.Application.Commands.Staffs.CreateStaff
+{
+    public class CreateStaffCommand : IRequest<int>
+    {
+        public string Name { get; set; }
+
+        public CreateStaffCommand(string name)
+        {
+            Name = name;
+        }
+    }
+}
