cc8cccafdc44d3e218478b1310ddceb07e728048
anh.ton <anh.ton@aegona.com>
2024-05-30 17:57:57 +0700

    implement demo rabbitmq commandservice

---
+++ CommandService/EventPublisher.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+
+using Shared;
+
+using System.Text;
+using System.Text.Json;
+
+namespace CommandService
+{
+    public class EventPublisher
+    {
+        private readonly IModel _channel;
+        private readonly string _exchangeName = "order_exchange";
+
+        public EventPublisher()
+        {
+            var factory = new ConnectionFactory() {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            var connection = factory.CreateConnection();
+            _channel = connection.CreateModel();
+            _channel.ExchangeDeclare(exchange: _exchangeName, type: ExchangeType.Fanout);
+        }
+
+        public void PublishOrderCreatedEvent(OrderCreatedEvent orderCreatedEvent)
+        {
+            var message = JsonSerializer.Serialize(orderCreatedEvent);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            _channel.BasicPublish(exchange: _exchangeName,
+                                  routingKey: "",
+                                  basicProperties: null,
+                                  body: body);
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+    }
+}
---
+++ Shared/OrderCreatedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared
+{
+    public class OrderCreatedEvent
+    {
+        public int OrderId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
