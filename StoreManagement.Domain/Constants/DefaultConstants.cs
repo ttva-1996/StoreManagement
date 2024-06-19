6dda011bff2d28b6e196045dfffa40e237896372
anh.ton <anh.ton@aegona.com>
2024-06-19 15:16:51 +0700

    insert script sql data country

---
+++ StoreManagement.Domain/Constants/DefaultConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace StoreManagement.Domain.Constants
+{
+    public class DefaultConstants
+    {
+        public const int DEFAULT_COUNTRY_VN = 243;
+    }
+}
---
+++ StoreManagement.Infrastructure/Migrations/20240619080447_Insert-Data-Table-Country.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertDataTableCountry : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sqlFile = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"Migrations/StaticData/countries.sql");
+            var script = File.ReadAllText(sqlFile);
+            migrationBuilder.Sql(script);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
