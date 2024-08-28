7fe8c56a3e0043e5e8f0448d116edc63ba856938
anh.ton <anh.ton@aegona.com>
2024-08-28 15:03:03 +0700

    change staffservices using axios instance

--- storemanagementreact/store-management-typescript/src/services/staff/StaffServices.ts
+++ storemanagementreact/store-management-typescript/src/services/staff/StaffServices.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axiosInstance from '../../utils/axiosConfig';
 
 const API_URL = 'https://localhost:44372/api/staff';
 
@@ -10,27 +10,27 @@ export interface Staff {
 
 class StaffService {
   async getStaffs(): Promise<Staff[]> {
-    const response = await axios.get<Staff[]>(API_URL);
+    const response = await axiosInstance.get<Staff[]>(API_URL);
     return response.data;
   }
 
   async getStaff(id: any): Promise<Staff> {
-    const response = await axios.get<Staff>(`${API_URL}/${id}`);
+    const response = await axiosInstance.get<Staff>(`${API_URL}/${id}`);
     return response.data;
   }
 
   async createStaff(staff: Omit<Staff, 'id'>): Promise<Staff> {
-    const response = await axios.post<Staff>(API_URL, staff);
+    const response = await axiosInstance.post<Staff>(API_URL, staff);
     return response.data;
   }
 
   async updateStaff(id: any, staff: Staff): Promise<Staff> {
-    const response = await axios.put<Staff>(`${API_URL}/${id}`, staff);
+    const response = await axiosInstance.put<Staff>(`${API_URL}/${id}`, staff);
     return response.data;
   }
 
   async deleteStaff(id: any): Promise<void> {
-    await axios.delete(`${API_URL}/${id}`);
+    await axiosInstance.delete(`${API_URL}/${id}`);
   }
 }
 
