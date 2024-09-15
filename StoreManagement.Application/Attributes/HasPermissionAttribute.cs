64e699fe57ebc6359ba22235734082661fffa100
anh.ton <anh.ton@aegona.com>
2024-09-15 11:06:08 +0700

    implement haspermissionattribute

---
+++ StoreManagement.Application/Attributes/HasPermissionAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+using StoreManagement.Domain.Enums;
+
+namespace StoreManagement.Application.Attributes;
+
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class, Inherited = false)]
+public class HasPermissionAttribute : AuthorizeAttribute
+{
+    public HasPermissionAttribute(EnumPermission permission) : base(permission.ToString())
+    {
+    }
+}
---
+++ StoreManagement.Domain/Enums/EnumPermission.cs
@@ -0,0 +1,14 @@
+﻿namespace StoreManagement.Domain.Enums;
+
+public enum EnumPermission
+{
+    ADMIN = 2000,
+    VIEW_STAFF = 1,
+    CREATE_STAFF = 2,
+    UPDATE_STAFF = 3,
+    DELETE_STAFF = 4,
+    VIEW_CUSTOMER = 5,
+    CREATE_CUSTOMER = 6,
+    UPDATE_CUSTOMER = 7,
+    DELETE_CUSTOMER = 8,
+}
